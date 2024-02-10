7f907b059c5279a7d362ac57ae8a113ee765f8b7
sebalp100 <sebalp100@gmail.com>
2024-02-10 18:14:48 -0300

    Update post contact

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,7 +1,9 @@
 const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
 
 const getContacts = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get all contact" });
+  const contacts = await Contact.find();
+  res.status(200).json(contacts);
 });
 
 const createContact = asyncHandler(async (req, res) => {
@@ -10,7 +12,13 @@ const createContact = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("All fields must be completed")
   }
-  res.status(201).json({ message: "Create a contact" });
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  });
+
+  res.status(201).json(contact);
 });
 
 const getContact = asyncHandler(async (req, res) => {
