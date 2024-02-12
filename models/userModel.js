32cd8f55eee48c708f502aa14e48b56d108b418e
sebalp100 <sebalp100@gmail.com>
2024-02-12 18:26:09 -0300

    Add user model

---
+++ models/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const userScheme = mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please add a username"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please add the user email address"],
+    unique: [true, "Email address already taken"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please add the user password"],
+  },
+},
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
