260c0c3f4c78013b0a5d63370dae1d9e1e74e652
sebalp100 <sebalp100@gmail.com>
2024-02-19 15:46:20 -0300

    Add helmet and cors

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const express = require("express");
 const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
+const helmet = require("helmet");
+const cors = require("cors");
 
 connectDb();
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+app.use(helmet());
+app.use(cors({
+  origin: 'http://localhost:5001',
+  optionsSuccessStatus: 200
+}));
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
